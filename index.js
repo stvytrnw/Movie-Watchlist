5134ac38e3f9aa42057f26e60614b9b44075c590
L3RiZ <107563807+L3RiZ@users.noreply.github.com>
2023-02-27 19:31:37 +0100

    #3

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let moviesArray = [];
 
 document.getElementById('search-bar').addEventListener('submit', e => {
     e.preventDefault();
-    document.getElementById("main").innerHTML = ''
     moviesArray = []
 
     fetch(`http://www.omdbapi.com/?s=${document.getElementById('search-input').value}&apikey=72c0f766`)
@@ -13,21 +12,29 @@ document.getElementById('search-bar').addEventListener('submit', e => {
                     .then(res => res.json())
                     .then(data => {
                         moviesArray.push(data);
+                        renderMovies();
+                        console.log(data)
                     })
             })
         })
-
-    renderMovies();
-    document.getElementById('search-input').value = ''
 })
 
 function renderMovies(){
+    document.getElementById("main").innerHTML = ''
+
     moviesArray.map(movie => {
         document.getElementById("main").innerHTML += `
         <div class="movie-cnt">
             <img class="movie-poster" src="${movie.Poster}" alt="movie poster"/>
             <div class="movie-description-cnt">
                 <h2>${movie.Title}</h2>
+                <p class="rating">${movie.imdbRating}</p>
+                <div class="details-row">
+                    <p class="runtime">${movie.Runtime}</p>
+                    <p class="genre">${movie.Genre}</p>
+                    <button id="add-watchlist" class="watchlist add"></button>
+                </div>
+                <p class="plot">${movie.Plot}</p>
             </div>
         </div>`
     })
